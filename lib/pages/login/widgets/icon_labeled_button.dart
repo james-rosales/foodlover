8b8c4684ec97c556b8837e8538b6fcc825624310
James Carlo <r_jamescarlo31@yahoo.com>
2022-11-10 10:20:54 +0800

    refactor: add overlaycolor on buttons

--- lib/pages/login/widgets/icon_labeled_button.dart
+++ lib/pages/login/widgets/icon_labeled_button.dart
@@ -4,11 +4,13 @@ class IconLabeledButton extends StatelessWidget {
   final Widget? icon;
   final String label;
   final Color backgroundColor;
+  final Color overlayColor;
   const IconLabeledButton(
       {super.key,
       this.icon,
       required this.label,
-      required this.backgroundColor});
+      required this.backgroundColor,
+      required this.overlayColor});
 
   @override
   Widget build(BuildContext context) {
@@ -20,11 +22,18 @@ class IconLabeledButton extends StatelessWidget {
         width: 200,
         height: 50,
         child: ElevatedButton.icon(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: backgroundColor,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(
-                30,
+          style: ButtonStyle(
+            overlayColor: MaterialStateProperty.all(
+              overlayColor,
+            ),
+            backgroundColor: MaterialStateProperty.all(
+              backgroundColor,
+            ),
+            shape: MaterialStateProperty.all(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(
+                  30,
+                ),
               ),
             ),
           ),
