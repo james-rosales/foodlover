92097ce20e39fa47b22354020f4e5ade1f2853cb
James Carlo <r_jamescarlo31@yahoo.com>
2022-11-10 11:29:22 +0800

    refactor: delete overlay color and add size

--- lib/pages/login/widgets/icon_labeled_button.dart
+++ lib/pages/login/widgets/icon_labeled_button.dart
@@ -4,43 +4,44 @@ class IconLabeledButton extends StatelessWidget {
   final Widget? icon;
   final String label;
   final Color backgroundColor;
-  final Color overlayColor;
-  const IconLabeledButton(
-      {super.key,
-      this.icon,
-      required this.label,
-      required this.backgroundColor,
-      required this.overlayColor});
+  final double width;
+  final double height;
+
+  const IconLabeledButton({
+    super.key,
+    this.icon,
+    required this.label,
+    required this.backgroundColor,
+    required this.width,
+    required this.height,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(
-        top: 15,
-      ),
+      padding: const EdgeInsets.only(top: 20, right: 10, left: 10),
       child: SizedBox(
-        width: 200,
-        height: 50,
-        child: ElevatedButton.icon(
-          style: ButtonStyle(
-            overlayColor: MaterialStateProperty.all(
-              overlayColor,
-            ),
-            backgroundColor: MaterialStateProperty.all(
-              backgroundColor,
-            ),
-            shape: MaterialStateProperty.all(
-              RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(
-                  30,
+        width: width,
+        height: height,
+        child: SizedBox(
+          child: ElevatedButton.icon(
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(
+                backgroundColor,
+              ),
+              shape: MaterialStateProperty.all(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(
+                    5,
+                  ),
                 ),
               ),
             ),
-          ),
-          onPressed: () {},
-          icon: icon!,
-          label: Text(
-            label,
+            onPressed: () {},
+            icon: icon!,
+            label: Text(
+              label,
+            ),
           ),
         ),
       ),
