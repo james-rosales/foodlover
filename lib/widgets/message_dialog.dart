8f7cd029abde4421dd47e0300c2354f4903c7183
James Carlo <r_jamescarlo31@yahoo.com>
2022-11-10 09:38:30 +0800

    refactor: widget for message dialog

---
+++ lib/widgets/message_dialog.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class MessageDialog extends StatelessWidget {
+  final String title;
+  final Widget content;
+
+  const MessageDialog({
+    super.key,
+    required this.content,
+    required this.title,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return SimpleDialog(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      title: Padding(
+        padding: const EdgeInsets.only(bottom: 20.0),
+        child: Text(
+          title,
+          textAlign: TextAlign.center,
+        ),
+      ),
+      children: [
+        SizedBox(
+          width: 310,
+          height: 120,
+          child: Column(
+            children: [content],
+          ),
+        )
+      ],
+    );
+  }
+}
