948614418982f40f826d33549d5d92760b54bcc1
James Carlo <r_jamescarlo31@yahoo.com>
2022-11-14 11:07:07 +0800

    refactor: add tabpress event

--- lib/pages/orders/view/orders_app_bar.dart
+++ lib/pages/orders/view/orders_app_bar.dart
@@ -1,18 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:foodbank/pages/dashboard/dashboard.dart';
 
 class OrdersAppBar extends StatelessWidget {
   const OrdersAppBar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return AppBar(
-      elevation: 0.0,
-      leading: IconButton(
-        onPressed: () {},
-        icon: const Icon(
-          Icons.chevron_left,
+    var bloc = context.read<DashboardBloc>();
+    return BlocBuilder<DashboardBloc, DashboardState>(
+        builder: (context, state) {
+      return AppBar(
+        elevation: 0.0,
+        leading: IconButton(
+          onPressed: () => bloc.add(
+            const DashboardTabPressed(
+              0,
+            ),
+          ),
+          icon: const Icon(
+            Icons.chevron_left,
+          ),
         ),
-      ),
-    );
+      );
+    });
   }
 }
